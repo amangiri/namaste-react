eaf659fbc23af39f2f0d8321f308b836139aa3ec
amangiri <aman.giri0104@gmail.com>
2023-06-24 20:39:36 +0530

    episode-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,46 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is namaste react dev series.</h2>
-    </div>
-  );
-};
+import { Component } from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    // console.log("About constructor");
+  }
+
+  render() {
+    // console.log("About render");
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is namaste react dev series.</h2>
+        {/* <User name={"Aman Giri"} location={"Delhi"} contact={"@amangiri18"} /> */}
+        <UserClass
+          name={"Akshay Saini"}
+          location={"Banguluru"}
+          contact={"@akshaymarch"}
+        />
+        {/* <UserClass
+          name={"Aman Giri"}
+          location={"Delhi"}
+          contact={"@amangiri_18"}
+        /> */}
+      </div>
+    );
+  }
+  componentDidMount() {
+    // console.log("parent componentDidMount");
+  }
+}
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About</h1>
+//       <h2>This is namaste react dev series.</h2>
+//       {/* <User name={"Aman Giri"} location={"Delhi"} contact={"@amangiri18"} /> */}
+//       <UserClass name={"Aman Giri"} location={"Delhi"} contact={"@amangiri_18"} />
+//     </div>
+//   );
+// };
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userinfo:{
+        name:"Lorem Ipsum",
+        location: "Lorem Ipsum",
+        avatar_url:""
+      }
+    };
+    console.log(this.props.name,"child constructor");
+  }
+
+  async componentDidMount() {
+    console.log("componentDidMount");
+    const data= await fetch("https://api.github.com/users/amangiri");
+    const json= await data.json();
+    console.log(json);
+    this.setState({
+      userinfo:json
+    })
+  }
+
+  render() {
+    // const { name, location, contact } = this.props;
+    const {name,location,avatar_url}= this.state.userinfo;
+    console.log("child render");
+    return (
+      <div className="user-card">
+        <img src={avatar_url} />
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+
+        {/* <h4>Contact: {contact}</h4> */}
+      </div>
+    );
+  }
+
+  componentDidUpdate(){
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount(){
+    console.log("componentWillUnmount");
+  }
+
+ 
+}
+
+export default UserClass;
+
+/****
+ * ----MOUNTING----
+ * 
+ * Constructor (dummy)
+ * Render (dummy)
+ *    HTML dummy
+ * component Did mount
+ *    Api call
+ *    set state-> state variable is updated
+ * 
+ * ----UPATE----
+ *        render api data
+ *        HTML new api data
+ * 
+ * component did update
+ */
